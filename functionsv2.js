043d1bdedada654a71f37aaea60b199e36f0ddcf
thebrunnalves <brunno_alvesferreira@yahoo.com.br>
2018-10-23 16:43:51 -0300

    Update functionsv2.js

--- functionsv2.js
+++ functionsv2.js
@@ -11,8 +11,8 @@ function discountOff() {
 }
 
 function shareThis() {
-    $("body.pagina-produto .produto-compartilhar").prepend($("<div class='sharethis-inline-share-buttons'></div>")), $(".sharethis-inline-share-buttons").append($("<script async>", {
-        src: "//platform-api.sharethis.com/js/sharethis.js#property=5bcf77f9e0bd050011105828&product=inline-share-buttons"
+    $("body.pagina-produto .produto-compartilhar").prepend($("<div class='sharethis-inline-share-buttons'></div>")), $(".sharethis-inline-share-buttons").append($("<script>", {
+        src: "//platform-api.sharethis.com/js/sharethis.js#property=5991eb27770096001434ea7d&product=inline-share-buttons"
     }))
 }
 
