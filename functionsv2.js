a15cdbd285451f2d65aa68cd23dac0bf5e6f8820
thebrunnalves <brunno_alvesferreira@yahoo.com.br>
2018-10-26 17:25:11 -0300

    Update functionsv2.js

--- functionsv2.js
+++ functionsv2.js
@@ -961,7 +961,7 @@ function abrigaConta(){
     $(".conteudo-topo .superior").find(".btn-group").closest(".span8").removeClass("span8").addClass("login-inside");
     $(".abriga-painel .abriga-conta").append($("#cabecalho .login-inside"));
     $(".login-inside").prepend('<a href="https://madfox.com.br/conta/login" class="bem-vindo cor-secundaria fas fa-user-circle"> <svg class="icon-novouser"><use xlink:href="#novouser"></use></svg><span class="cor-principal"></span>Minha<br>Conta</a>');
-    $(".login-inside .bt").remove();
+    $(".login-inside .btn-group").remove();
 
 }
 
